099b4912e406ce26008b4403b69f995c7a924f97
Ibragim Dzhiblavi <dzhiblavi@gmail.com>
2021-04-07 12:02:09 +0300

    bug in setup fix

--- setup.bat
+++ setup.bat
@@ -1,4 +1,5 @@
 mkdir test
 cd test
 git clone https://github.com/microsoft/vcpkg
+cd vcpkg
 bootstrap-vcpkg.bat
--- setup.sh
+++ setup.sh
@@ -4,4 +4,5 @@ set -e
 mkdir test
 cd test
 git clone https://github.com/microsoft/vcpkg
+cd vcpkg
 ./bootstrap-vcpkg.sh
